974633995e4b7d9aa09ed67eeebf4dc2d0b5f8e5
Wincent Colaiuta <win@wincent.com>
2010-11-02 08:45:06 +0100

    Reformat header (cosmetic only)
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOTestRunner/WOTestRunner.m
+++ WOTestRunner/WOTestRunner.m
@@ -1,23 +1,19 @@
+// WOTestRunner.m
+// WOTest
 //
-//  WOTestRunner.m
-//  WOTest
+// Copyright 2004-2009 Wincent Colaiuta.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  Created by Wincent Colaiuta on 12 March 2005.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  Copyright 2004-2007 Wincent Colaiuta.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // main header
 #import "WOTestRunner.h"
